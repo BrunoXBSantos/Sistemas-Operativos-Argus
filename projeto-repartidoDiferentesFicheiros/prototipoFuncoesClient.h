d1d0bbc1fb32e3436ddbef2a6651fb7cde26f5b5
BrunoXBSantos <brunobsantos.04@gmail.com>
2020-01-10 00:56:49 +0000

    Removendo pastas nao usadas

--- projeto-repartidoDiferentesFicheiros/prototipoFuncoesClient.h
+++
@@ -1,8 +0,0 @@
-void iniciaInterpretador();
-void getPrimeiraPalavra(char q[80], char primeiraPalavra[30]);
-void enviarComando(char fraseEnviar[80]);
-void historico();
-void ajuda();
-void listar();
-void alarme();
-int readln(int fildes, char *buf, int nbyte);
